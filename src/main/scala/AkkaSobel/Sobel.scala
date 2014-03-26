c16202a558aadd5df76310bd469d42b1a65cc189
Norbert Scheller <nscheller@actian.com>
2014-03-26 14:42:40 +0100

    gray, convolve, dilate Operator

--- src/main/scala/AkkaSobel/Sobel.scala
+++ src/main/scala/AkkaSobel/Sobel.scala
@@ -12,6 +12,7 @@ import java.awt.Color
 
 sealed trait OpMessage
 case object Start extends OpMessage
+case object Stop  extends OpMessage
 case class Ops(ops: List[akka.actor.ActorRef])
 case class Work(lineNo: Int) extends OpMessage
 case class Result(lineNo: Int, lineResult: Array[Int]) extends OpMessage
@@ -66,18 +67,47 @@ object Sobel extends App {
     val width  = srcImage.getWidth
     
     val FINISH_ID = 0
-    
-    val writer = context.actorOf(Props(new ImageWriter(start)), name = "imageWriter")
 
+    val embossKernel  = Array(Array(-1.0, -0.5,  0.0),
+                              Array(-0.5,  1.0,  0.5),
+                              Array( 0.0,  0.5,  1.0))
+    
+    val embossKernel2 = Array(Array(-2.0, -1.5, -1.0,  0.0,  0.0),
+                              Array(-1.5, -1.0, -0.5,  0.0,  0.0),
+                              Array(-1.0, -0.5,  1.0,  0.5,  1.0),
+                              Array( 0.0,  0.0,  0.5,  1.0,  1.5),
+                              Array( 0.0,  0.0,  1.0,  1.5,  2.0))
+    
+    val embossKernel3 = Array(Array(-3.0, -2.5, -2.0, -1.5,  0.0,  0.0,  0.0),
+                              Array(-2.5, -2.0, -1.5, -1.0,  0.0,  0.0,  0.0),
+                              Array(-2.0, -1.5, -1.0, -0.5,  0.0,  0.0,  0.0),
+                              Array(-1.5, -1.0, -0.5,  1.0,  0.5,  1.0,  1.5),
+                              Array( 0.0,  0.0,  0.0,  0.5,  1.0,  1.5,  2.0),
+                              Array( 0.0,  0.0,  0.0,  1.0,  1.5,  2.0,  2.5),
+                              Array( 0.0,  0.0,  0.0,  1.5,  2.0,  2.5,  3.0))
+    
+    val sharpenKernel = Array(Array(-1.0, -1.0, -1.0),
+                              Array(-1.0,  9.0, -1.0),
+                              Array(-1.0, -1.0, -1.0))
+    
     val tmpImage = new BufferedImage(srcImage.getWidth, srcImage.getHeight, BufferedImage.TYPE_INT_ARGB)
 
+    val writer = context.actorOf(Props(new ImageWriter(start)), name = "imageWriter")
+
     val sobelRouter    = context.actorOf(Props(new SobelOp(srcImage)).withRouter(RoundRobinRouter(noOfWorkers)), name = "sobelRouter")
+    val grayRouter     = context.actorOf(Props(new GrayOp(srcImage)).withRouter(RoundRobinRouter(noOfWorkers)), name = "grayRouter")
     val thresRouter    = context.actorOf(Props(new ThresholdOp(srcImage, threshold)).withRouter(RoundRobinRouter(noOfWorkers)), name = "thresRouter")
-    val sharpenRouter  = context.actorOf(Props(new SharpenOp(srcImage, 0.5)).withRouter(RoundRobinRouter(noOfWorkers)), name = "sharpenRouter")
+    val invertRouter   = context.actorOf(Props(new InvertOp(srcImage)).withRouter(RoundRobinRouter(noOfWorkers)), name = "invertRouter")
+    val dilateRouter   = context.actorOf(Props(new DilateOp(srcImage, 1, threshold)).withRouter(RoundRobinRouter(noOfWorkers)), name = "dilateRouter")
+    val sharpenRouter  = context.actorOf(Props(new SharpenOp(srcImage, 0.1)).withRouter(RoundRobinRouter(noOfWorkers)), name = "sharpenRouter")
     val blurRouter     = context.actorOf(Props(new BlurOp(srcImage)).withRouter(RoundRobinRouter(noOfWorkers)), name = "blurRouter")
+    val embossRouter   = context.actorOf(Props(new ConvolveOp(srcImage, embossKernel2)).withRouter(RoundRobinRouter(noOfWorkers)), name = "embossRouter")
+    val sharpOpRouter  = context.actorOf(Props(new ConvolveOp(srcImage, sharpenKernel)).withRouter(RoundRobinRouter(noOfWorkers)), name = "sharpOpRouter")
     val noOpRouter     = context.actorOf(Props(new NoOp(srcImage)).withRouter(RoundRobinRouter(noOfWorkers)), name = "noOpRouter")
     
-    var ops = List(blurRouter, blurRouter, blurRouter, blurRouter, blurRouter)
+//  var ops = List(blurRouter, sobelRouter, thresRouter, invertRouter)
+    var ops = List(blurRouter, sobelRouter, thresRouter, dilateRouter, invertRouter)
+//  var ops = List(blurRouter, sobelRouter, thresRouter, invertRouter, dilateRouter)
     
     override def receive = {
       case Start => {
@@ -85,6 +115,11 @@ object Sobel extends App {
         val router = ops.head
         ops = ops.tail
         for (i <- 0 until height) router ! Work(i)
+      }
+      
+      case Stop => {
+          println("Master Finish after " + (System.currentTimeMillis - start).millis)
+          context.system.shutdown
       }
       
       case Result(lineNo, lineResult) => {
@@ -103,8 +138,6 @@ object Sobel extends App {
             noOfLines = 0
             println("Step done after " + (System.currentTimeMillis - start).millis)
 
-            //writer ! Write(tmpImage, "tmp_" + fileName, 1)
-//            srcImage = tmpImage
             val graphics = srcImage.getGraphics
             graphics.drawImage(tmpImage, 0, 0, null)
             graphics.dispose
@@ -118,8 +151,7 @@ object Sobel extends App {
       
       case WriteFinished(id) => {
         if (id == FINISH_ID) {
-          println("Master Finish after " + (System.currentTimeMillis - start).millis)
-          context.system.shutdown
+          self ! Stop
         }
       }
     }
